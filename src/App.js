bc5fac547f70116c2d5740239d00e8e855d8616c
guilherme-dsveiga <guilherme.dsveiga@gmail.com>
2021-08-19 15:41:13 -0300

    Arrumando o design para celular

--- src/App.js
+++ src/App.js
@@ -38,20 +38,24 @@ function App() {
       <div className="upper-container">
         <h1 onClick={refreshPage}>React Clima</h1>
         <div className="form-container">
-          <label htmlFor="city">Digite o nome da cidade:</label>
-          <input id="input" onKeyPress={handleKeypress} ref={cityInput} type="text" name="city"></input>
+          <div className="label-input-container">
+            <label htmlFor="city">Digite o nome da cidade:</label>
+            <input id="input" onKeyPress={handleKeypress} ref={cityInput} type="text" name="city"></input>
+          </div>
           <button type="submit" onClick={callFetch}>+</button>
         </div>
-        <div className="cards-container">
-          {weatherData.map((item,key) => (
-          <Card key={key} isRender={renderState} name={item.name} main={item.weather ? item.weather[0].main : ''} temp={item.main ? item.main.temp : ''} min={item.main ? item.main.temp_min : ''} max={item.main ? item.main.temp_max : ''} />
-          ))}
+        <div className="space-between">
+          <div className="cards-container">
+            {weatherData.map((item, key) => (
+              <Card key={key} isRender={renderState} name={item.name} main={item.weather ? item.weather[0].main : ''} temp={item.main ? item.main.temp : ''} min={item.main ? item.main.temp_min : ''} max={item.main ? item.main.temp_max : ''} />
+            ))}
+          </div>
+          <footer>
+            Feito com <span role="img" aria-label="coração">❤️</span> por Guilherme Veiga<br />
+            Aplicativo feito para a finalidade de estudo da biblioteca ReactJS <br />
+          </footer>
         </div>
       </div>
-      <footer>
-        Feito com <span role="img" aria-label="coração">❤️</span> por Guilherme Veiga<br />
-        Aplicativo feito para a finalidade de estudo da biblioteca ReactJS <br />
-      </footer>
     </div>
   );
 }
