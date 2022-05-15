7880cd519079edc5a1f23a2106d6c132d061b389
Guilherme Veiga <guilherme.veiga@fasters.com.br>
2022-05-15 01:34:36 -0300

    changing api key and base url to .env

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,7 @@
-const API_KEY = "abc1b31f604e9f2dd20c699d0fafbdef";
-const API_BASE = "https://api.openweathermap.org/data/2.5/";
-
-const basicFetch = async (endpoint) => {
-  const req = await fetch(`${API_BASE}${endpoint}`);
+const apiClient = async (endpoint) => {
+  const req = await fetch(
+    `${process.env.REACT_APP_PUBLIC_BASE_URL}${endpoint}`
+  );
   const json = req.json();
   return json;
 };
@@ -12,7 +11,9 @@ const Weather = {
     let weatherListData = [];
     for (let i = 0; i < param.length; i++) {
       weatherListData.push(
-        await basicFetch(`weather?q=${param[i]}&appid=${API_KEY}&units=metric`)
+        await apiClient(
+          `weather?q=${param[i]}&appid=${process.env.REACT_APP_PUBLIC_API_KEY}&units=metric`
+        )
       );
     }
     return weatherListData;
